a64884313fa224c1b5fe2ee13bb691f8624bb1ac
Tiimmii <danieloluwoleoluwatimileyin@gmail.com>
2025-07-17 10:01:49 +0100

    setting up customer controller

--- src/main/java/com/iam/iam_app/controller/CustomerController.java
+++ src/main/java/com/iam/iam_app/controller/CustomerController.java
@@ -1,5 +1,24 @@
 package com.iam.iam_app.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.iam.iam_app.response.ResourceResponse;
+import com.iam.iam_app.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
-    
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/resource")
+    public ResponseEntity<List<ResourceResponse>> getAllResource(){
+        List<ResourceResponse> resources = customerService.getAllResource();
+        return ResponseEntity.ok(resources); 
+    }
 }
