be5883daa2328a64b64eabc4bd166c63fe52fb4d
Tiimmii <danieloluwoleoluwatimileyin@gmail.com>
2025-07-17 10:13:51 +0100

    Setting up other services and implementations

--- src/main/java/com/iam/iam_app/implementation/CustomerServiceImpl.java
+++ src/main/java/com/iam/iam_app/implementation/CustomerServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.iam.iam_app.entity.Resource;
 import com.iam.iam_app.repositories.ResourceRepository;
 import com.iam.iam_app.response.ResourceResponse;
 import com.iam.iam_app.service.CustomerService;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
     @Autowired
     private ResourceRepository resourceRepository;
